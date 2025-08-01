903308931643258a38aed65bf729b4a5a25cf1f9
itc-s24021 <s24021@std.it-college.ac.jp>
2025-08-01 10:29:00 +0900

    課題

--- D53Q21.java
+++ D53Q21.java
@@ -1,8 +1,8 @@
 public class D53Q21 {
     public static void main(String[] args) {
-        byte a = 0b00111111; // 2進数リテラル（63）
-        byte b = 0100;       // 8進数リテラル（64）
-        byte c = 0x41;       // 16進数リテラル（65）
+        byte a = 0b00111111;
+        byte b = 64;
+        byte c = 0x41;
 
         System.out.println("a = " + a); // 63
         System.out.println("b = " + b); // 64
