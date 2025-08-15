71fdfc29dd179450b96ac60656cb22af5ff0b772
itc-s24021 <s24021@std.it-college.ac.jp>
2025-08-15 10:42:32 +0900

    課題

---
+++ D51Q20.java
@@ -0,0 +1,7 @@
+public class D51Q20 {
+    public static void main(String[] args) {
+        int score = 85;
+        String result = (score >= 70) ? "合格" : "不合格";
+        System.out.println(result);
+    }
+}
